2d7fa8684bb45821f67195713fbb9e6ac47fc9cf
YOUR_NAME <YOUR_EMAIL>
2025-10-07 15:54:28 +0200

    kmt

--- Exercise 6.py
+++ Exercise 6.py
@@ -87,7 +87,7 @@ for i in range(10):
     values.append(number)
 
     
-print(values, "\n")
+print(values, "\n") #\n lager mellom me
 
 
 #Oppgave 3
@@ -110,7 +110,7 @@ for number in values:
         count = count + 1
     
 
-"""
+
 
 #b2)
 
@@ -118,3 +118,27 @@ for number in values:
 for number in values:
     print(sum(values))
     break
+
+
+
+#4
+even=[] #definere liste for even tall
+odd=[] #definere liste for odd tall
+for number in values: # går igjennom hvert tall i lista
+    if number %2 == 0: # hvis tallet er even
+        even.append(number) #legges det til i even lista
+    else: #hvis ikke 
+        odd.append(number) #legges den inn i oss lista
+print("Even list",even,"Odd list",odd) #printer ut begge listene
+
+"""
+#5
+count = 0
+if count < 10:
+    for number in values: # går igjennom hvert tall i lista    
+        if number %2 != 0: # hvis tallet er even
+            values.remove(number)
+            count += 1
+
+print(values)
+  
